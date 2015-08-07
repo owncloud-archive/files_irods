2602d3a9a865f3c2b87dcce43e3802d23e153784
Nick Young <nick.young@auckland.ac.nz>
2015-08-07 17:26:46 +1200

    add remove button

--- ajax/getMeta.php
+++ ajax/getMeta.php
@@ -15,11 +15,16 @@ $html = "<table id='metadata'>
                <th>Key</th>
                <th>Value</th>
                <th>Units</th>
+               <th>Actions</th>
              </tr>
            </thead>
            <tbody>";
 foreach ($meta as $m) {
-  $html .= sprintf("<tr><td data-name='name' data-pk=$m->id>%s</td><td data-name='value' data-pk=$m->id>%s</td><td data-name='units' data-pk=$m->id>%s</td></tr>", $m->name, $m->value, $m->units);
+  $html .= sprintf("<tr><td class='editable' data-name='name' data-pk=$m->id>%s</td>
+                    <td class='editable' data-name='value' data-pk=$m->id>%s</td>
+                    <td class='editable' data-name='units' data-pk=$m->id>%s</td>
+                    <td><button class='btn btn-danger remove-metadata' data-pk=$m->id><i class='glyphicon glyphicon-remove'></i> Remove</button></td>
+                    </tr>", $m->name, $m->value, $m->units);
 }
 $html .= "</tbody></table>";
 
--- js/meta.js
+++ js/meta.js
@@ -1,3 +1,8 @@
+function remove(id) {
+  console.log("Delete "+id);
+  $("table#metadata>tbody>tr>td.editable[data-pk='"+id+"']").parent().remove();
+}
+
 function loadContent(filename) {
   $.getJSON(OC.filePath('files_irods', 'ajax', 'getMeta.php'),
             {source:filename},
@@ -12,16 +17,24 @@ function loadContent(filename) {
                   mode: 'inline',
                   success: function (response, newValue) {
                     if (response.newId && response.oldId) {
-                      $("td[data-pk='" + response.oldId + "']").editable('option', 'pk', response.newId);
+                      $("table#metadata>tbody>tr>td.editable[data-pk='" + response.oldId + "']").attr('data-pk', response.newId);
+                      $("table#metadata>tbody>tr>td>button.remove-metadata[data-pk='" + response.oldId + "']").attr('data-pk', response.newId);
+                      $("table#metadata>tbody>tr>td.editable[data-pk='" + response.oldId + "']").editable('option', 'pk', response.newId);
                     }
                   }
               };
-              $('#metadata>tbody>tr>td').editable(options);
+              $('table#metadata>tbody>tr>td.editable').editable(options);
+              $('table#metadata>tbody>tr>td>button.remove-metadata').click(function() {
+                remove($(this).attr('data-pk'));
+              });
               $('.oc-dialog-content').append('<button id="add-metadata" class="btn btn-primary"><i class="glyphicon glyphicon-plus"></i> Add</button>');
               $("#add-metadata").click(function() {
                 var id = Date.now();
-                $("#metadata>tbody").append("<tr><td data-name='name' data-pk='"+id+"'>name</td><td data-name='value' data-pk='"+id+"'>value</td><td data-name='units' data-pk='"+id+"'>units</td></tr>");
-                $("td[data-pk='" + id + "']").editable(options);
+                $("table#metadata>tbody").append("<tr><td class='editable' data-name='name' data-pk='"+id+"'>name</td><td class='editable' data-name='value' data-pk='"+id+"'>value</td><td class='editable' data-name='units' data-pk='"+id+"'>units</td><td><button class='btn btn-danger remove-metadata' data-pk='"+id+"'><i class='glyphicon glyphicon-remove'></i> Remove</button></td></tr>");
+                $("td[data-pk='" + id + "'].editable").editable(options);
+                $("table#metadata>tbody>tr>td>button.remove-metadata[data-pk='"+id+"']").click(function() {
+                  remove($(this).attr('data-pk'));
+                });
               });
               $('.oc-dialog-content').append('<button id="refresh-metadata" class="btn btn-primary"><i class="glyphicon glyphicon-refresh"></i> Refresh</button>');
               $("#refresh-metadata").click(function() {
